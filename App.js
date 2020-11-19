bfe20265968417992875d6f9eed25bb86877a8a7
Malik <malik43213@gmail.com>
2020-11-19 11:06:27 +0100

    app loading added

--- App.js
+++ App.js
@@ -1,14 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect} from 'react';
+import {AppLoading} from 'expo';
 import AppScreens from './app/screens/AppScreens';
 import { AuthProvider } from './app/context/auth/authContext';
+import { OrdersProvider } from './app/context/orders/ordersContext';
+import {getToken} from './app/context/tokenStorage';
+
 
 const App = () => {
-  // const [isLoading, setIsLoading] = useState(true);
+  const [isAppReady, setIsAppReady] = useState(false);
+
+  const wait = () => {
+    setTimeout(() => {
+      console.log('APP IS READY...')
+    }, 3000);
+  }
+  
 
   // return isLoading ? <WelcomeScreen /> : <LoginScreen />;
+return(
+  <>
+  {!isAppReady 
+  ?
+    <AppLoading  
+        startAsync={wait}
+        onFinish={() => setIsAppReady(true)}
+        onError={console.warn} />
+        :
+          <AuthProvider>
+          <OrdersProvider>
+            <AppScreens />
+          </OrdersProvider>
+        </AuthProvider>
+  }
+  </>
+)
+
+
   return (
     <AuthProvider>
-      <AppScreens />
+      <OrdersProvider>
+        <AppScreens />
+      </OrdersProvider>
     </AuthProvider>
   );
 };
