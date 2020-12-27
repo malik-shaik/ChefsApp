3f5a38af8119b94825b73516c3d862c554cfb7a3
Malik <malik43213@gmail.com>
2020-12-27 12:42:20 +0100

    disabled notifications

--- App.js
+++ App.js
@@ -7,49 +7,12 @@ import { OrdersProvider } from './app/context/orders/ordersContext';
 import { getToken } from './app/context/tokenStorage';
 import { NotificationProvider } from './app/context/notifications/notificationContext';
 import Constants from 'expo-constants';
-// import * as Notifications from 'expo-notifications';
 import * as Permissions from 'expo-permissions';
 import { setNotifications, registerNotifications } from './app/Notifications';
 import PushNotifications from './app/components/PushNotifications';
 
-// Notifications.setNotificationHandler({
-//   handleNotification: async () => ({
-//     shouldShowAlert: true,
-//     shouldPlaySound: true,
-//     shouldSetBadge: true
-//   })
-// });
-
 const App = () => {
   const [isAppReady, setIsAppReady] = useState(false);
-  // const [expoPushToken, setExpoPushToken] = useState('');
-  // const [notification, setNotification] = useState(false);
-  // const notificationListener = useRef();
-  // const responseListener = useRef();
-
-  // useEffect(() => {
-  //   // registerForPushNotificationsAsync().then((token) => {
-  //   registerNotifications().then((token) => {
-  //     console.log('TOKEN :', token);
-  //     setExpoPushToken(token);
-  //   });
-
-  //   // This listener is fired whenever a notification is received while the app is OPEN or foregrounded
-  //   notificationListener.current = Notifications.addNotificationReceivedListener((notification) => {
-  //     console.log('APP IS FOREGROUND (OPEN) AND RECIVED NOTIFICATION');
-  //     setNotification(notification);
-  //   });
-
-  //   // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
-  //   responseListener.current = Notifications.addNotificationResponseReceivedListener((response) => {
-  //     console.log('RESPONSE :', response);
-  //   });
-
-  //   return () => {
-  //     Notifications.removeNotificationSubscription(notificationListener);
-  //     Notifications.removeNotificationSubscription(responseListener);
-  //   };
-  // }, []);
 
   const wait = () => {
     setTimeout(() => {
@@ -57,7 +20,6 @@ const App = () => {
     }, 3000);
   };
 
-  // return isLoading ? <WelcomeScreen /> : <LoginScreen />;
   return (
     <>
       {!isAppReady ? (
@@ -73,14 +35,6 @@ const App = () => {
         </NotificationProvider>
       )}
     </>
-  );
-
-  return (
-    <AuthProvider>
-      <OrdersProvider>
-        <AppScreens />
-      </OrdersProvider>
-    </AuthProvider>
   );
 };
 
