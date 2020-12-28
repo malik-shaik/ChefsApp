052e8da338c3c61a9f5222207bbd1ec28ab8810e
Malik <malik43213@gmail.com>
2020-12-28 10:57:37 +0100

    fixed api

--- app/api/api.js
+++ app/api/api.js
@@ -2,8 +2,9 @@ import { create } from 'apisauce';
 
 const api = create({
   // baseURL: 'http://localhost:5000/',
-  baseURL: 'http://192.168.0.107:5000/',
+  // baseURL: 'http://192.168.0.107:5000/',
   // baseURL: 'https://chefmenodeserver.herokuapp.com/',
+  baseURL: 'https://polite-grasshopper-18.loca.lt/',
   headers: { 'content-type': 'application/json' }
 });
 
